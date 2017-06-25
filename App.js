fb5a17e3d25ce2de26da643f42f78f7ab02418fd
cyrilf <cyrilf7x@gmail.com>
2017-06-25 16:05:56 +0200

    Improve the dev config

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default class App extends React.Component {
     await Expo.Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf')
+      Ionicons: require('@expo/vector-icons/fonts/Ionicons.ttf'),
     })
 
     this.setState({ isReady: true })
