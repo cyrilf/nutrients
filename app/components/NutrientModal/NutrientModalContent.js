95ee63fe24417ebf34110466d7c95ef547a8f1fe
cyrilf <cyrilf7x@gmail.com>
2017-06-25 21:05:23 +0200

    Update the modal style

--- app/components/NutrientModal/NutrientModalContent.js
+++ app/components/NutrientModal/NutrientModalContent.js
@@ -1,43 +1,74 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { List, ListItem, Text, Card, CardItem, Button, H3, Body } from 'native-base'
+import { View, ScrollView, StyleSheet } from 'react-native'
+import { Text, Button, H3 } from 'native-base'
 
 const NutrientModalContent = ({ item, onClose }) => {
 
   return (
-    <Card>
-      <CardItem header>
-        <H3>
-          {item.name}
-        </H3>
-      </CardItem>
-      <CardItem>
-        <Body>
+    <View style={styles.card}>
+      <View>
+        <H3 style={stylesNB.cardHeaderTitle}>{item.name}</H3>
+      </View>
+      <View style={styles.cardContent}>
+        <ScrollView>
           { !!item.notes &&
-            <Text>
-              Note: <Text>{item.notes}</Text>
-            </Text>
+            <View>
+              <Text style={stylesNB.cardContentNoteText}>{item.notes}</Text>
+            </View>
           }
-          { !!item.food &&
-            <List
-              dataArray={item.food}
-              renderRow={(food) => (<ListItem><Text key={food}>{food.trim()}</Text></ListItem>)}/>
-          }
-        </Body>
-      </CardItem>
-      <CardItem footer>
-        <Button rounded light onPress={onClose}>
+          { !!item.food && (
+            <View style={styles.cardContentFood}>
+              <ScrollView>
+                { item.food.map((food) => <Text key={food} style={stylesNB.cardContentFoodText}>{food.trim()}</Text>) }
+              </ScrollView>
+            </View>
+          )}
+        </ScrollView>
+      </View>
+      <View>
+        <Button full light onPress={onClose}>
           <Text>Go Back</Text>
         </Button>
-      </CardItem>
-    </Card>
+      </View>
+    </View>
   )
 }
-
 
 NutrientModalContent.propTypes = {
   item: PropTypes.object.isRequired,
   onClose: PropTypes.func.isRequired,
 }
+
+// native-base doesn't support StyleSheet atm..
+const stylesNB = {
+  cardHeaderTitle: {
+    padding: 20,
+    textAlign: 'center',
+  },
+  cardContentNoteText: {
+    textAlign: 'center',
+    color: 'grey',
+    paddingBottom: 20,
+  },
+  cardContentFoodText: {
+    textAlign: 'center',
+  },
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  cardContent: {
+    alignSelf: 'stretch',
+    padding: 20,
+    flex: 1,
+  },
+  cardContentFood: {
+    justifyContent: 'center',
+  },
+})
 
 export default NutrientModalContent
