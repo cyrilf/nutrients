bcd0ffbd0f020cc59faead03ae6e85ab398c3a8a
cyrilf <cyrilf7x@gmail.com>
2017-06-27 22:27:22 +0200

    Eject the native base theme

--- app/containers/Root.js
+++ app/containers/Root.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { Container, Content } from 'native-base'
+import { Container, Content, StyleProvider } from 'native-base'
 
+import getTheme from 'native-base-theme/components'
+import commonColor from 'native-base-theme/variables/commonColor'
 import Header from 'components/anatomy/Header'
 import Footer from 'components/anatomy/Footer'
 import Home from './Home'
@@ -11,13 +13,15 @@ import nutrientsData from 'ressources/data/nutrients.json'
 export default class Root extends Component {
   render() {
     return (
-      <Container>
-        <Header />
-        <Content>
-          <Home nutrients={nutrientsData.nutrients}/>
-        </Content>
-        <Footer visible={false} />
-      </Container>
+      <StyleProvider style={getTheme(commonColor)}>
+        <Container>
+          <Header />
+          <Content>
+            <Home nutrients={nutrientsData.nutrients}/>
+          </Content>
+          <Footer visible={false} />
+        </Container>
+      </StyleProvider>
     )
   }
 }
